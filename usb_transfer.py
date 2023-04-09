f1d2500ebf27ccde89412118e3eb3f588d6eee59
amasarac <bm801643@gmail.com>
2023-04-09 14:01:35 -0500

    Update usb_transfer.py
    added ssl/tls functionality
    added smb support for usb storage use.

--- usb_transfer.py
+++ usb_transfer.py
@@ -1,6 +1,7 @@
 import cv2
 import pyaudio
 import socket
+import ssl
 import struct
 import sys
 import time
@@ -10,33 +11,122 @@ from zeroconf import Zeroconf, ServiceInfo
 import evdev
 from evdev import ecodes, InputDevice
 from selectors import DefaultSelector, EVENT_READ
+import os
+import tempfile
+from OpenSSL import crypto
 
 # Constants
-IP_ADDRESS = '0.0.0.0'
-PORT = 12345
-AUDIO_CHUNK_SIZE = 1024
-VIDEO_WIDTH = 640
-VIDEO_HEIGHT = 480
+IP_ADDRESS = "0.0.0.0"  # Change this to your Linux machine's IP address
+AUDIO_PORT = 8000
+VIDEO_PORT = 8001
+INPUT_DEVICES_PORT = 8002
 
-def transfer_audio_stream(audio_device_index):
-    # Create a PyAudio object for audio stream capture
-    audio = pyaudio.PyAudio()
-    stream = audio.open(format=pyaudio.paInt16,
-                        channels=2,
-                        rate=44100,
-                        input=True,
-                        frames_per_buffer=AUDIO_CHUNK_SIZE,
-                        input_device_index=audio_device_index)
+# Added for Samba share
+SAMBA_PORT = 445
 
-    # Create a socket for audio stream transfer
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((IP_ADDRESS, PORT + 1))
+CERT_FILE = "cert.pem"
+KEY_FILE = "key.pem"
 
+def generate_self_signed_cert():
+    key = crypto.PKey()
+    key.generate_key(crypto.TYPE_RSA, 2048)
+
+    cert = crypto.X509()
+    cert.get_subject().C = "US"
+    cert.get_subject().ST = "California"
+    cert.get_subject().L = "San Francisco"
+    cert.get_subject().O = "Example"
+    cert.get_subject().OU = "USB Transfer"
+    cert.get_subject().CN = "localhost"
+    cert.set_serial_number(1000)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(365 * 24 * 60 * 60)
+    cert.set_issuer(cert.get_subject())
+    cert.set_pubkey(key)
+    cert.sign(key, "sha256")
+
+    with tempfile.NamedTemporaryFile(delete=False) as f:
+        f.write(crypto.dump_privatekey(crypto.FILETYPE_PEM, key))
+        key_file = f.name
+
+    with tempfile.NamedTemporaryFile(delete=False) as f:
+        f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+        cert_file = f.name
+
+    return key_file, cert_file
+
+def register_audio_stream():
+    zeroconf = Zeroconf()
+    service_type = "_audio._tcp.local."
+    name = "USB Audio Stream"
+    port = AUDIO_PORT
+    service_info = ServiceInfo(service_type, f"{name}.{service_type}", addresses=[socket.inet_aton(IP_ADDRESS)], port=port)
+    zeroconf.register_service(service_info)
+    return zeroconf, service_info
+
+def register_video_stream():
+    zeroconf = Zeroconf()
+    service_type = "_video._tcp.local."
+    name = "USB Video Stream"
+    port = VIDEO_PORT
+    service_info = ServiceInfo(service_type, f"{name}.{service_type}", addresses=[socket.inet_aton(IP_ADDRESS)], port=port)
+    zeroconf.register_service(service_info)
+    return zeroconf, service_info
+
+def register_input_devices():
+    zeroconf = Zeroconf()
+    service_type = "_input._tcp.local."
+    name = "USB Input Devices"
+    port = INPUT_DEVICES_PORT
+    service_info = ServiceInfo(service_type, f"{name}.{service_type}", addresses=[socket.inet_aton(IP_ADDRESS)], port=port)
+    zeroconf.register_service(service_info)
+    return zeroconf, service_info
+
+# New function for registering the Samba share using Zeroconf
+def register_samba_share():
+    zeroconf = Zeroconf()
+    service_type = "_smb._tcp.local."
+    name = "USB Drive Share"
+    port = SAMBA_PORT
+    service_info = ServiceInfo(service_type, f"{name}.{service_type}", addresses=[socket.inet_aton(IP_ADDRESS)], port=port, properties={"path": "/media/usb"})
+    zeroconf.register_service(service_info)
+
+    return zeroconf, service_info
+
+def transfer_audio_stream():
+    # Set up audio stream
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16,
+                    channels=2,
+                    rate=44100,
+                    input=True,
+                    frames_per_buffer=1024)
+
+    # Create a secure socket for audio streaming
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((IP_ADDRESS, AUDIO_PORT))
+    sock.listen(1)
+    print(f"Listening for audio on {IP_ADDRESS}:{AUDIO_PORT}")
+
+    conn, address = sock.accept()
+    print(f"Accepted connection from {address}")
+
+    conn = ssl.wrap_socket(conn, keyfile=KEY_FILE, certfile=CERT_FILE, server_side=True)
+
+    # Stream audio to the connected client
+    try:
         while True:
-            data = stream.read(AUDIO_CHUNK_SIZE)
-            data_len = len(data)
-            sock.sendall(struct.pack('>I', data_len))
-            sock.sendall(data)
+            data = stream.read(1024)
+            conn.sendall(data)
+    except (BrokenPipeError, ConnectionResetError, OSError):
+        print("Audio stream disconnected")
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    conn.close()
+    sock.close()
 
 def transfer_video_stream(camera_device_index):
     # Open a video capture object for webcam video stream capture
@@ -57,99 +147,87 @@ def transfer_video_stream(camera_device_index):
             sock.sendall(data)
 
 def transfer_input_devices():
-    # Find USB keyboard and mouse devices
-    devices = [InputDevice(path) for path in evdev.list_devices()]
-    keyboard = None
-    mouse = None
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((IP_ADDRESS, INPUT_DEVICES_PORT))
+    sock.listen(1)
+    print(f"Listening for input devices on {IP_ADDRESS}:{INPUT_DEVICES_PORT}")
 
-    for device in devices:
-        if 'keyboard' in device.name.lower():
-            keyboard = device
-        if 'mouse' in device.name.lower():
-            mouse = device
-        if keyboard and mouse:
-            break
+    conn, address = sock.accept()
+    print(f"Accepted connection from {address}")
 
-    if not keyboard and not mouse:
-        print("No USB keyboard or mouse found.")
-        return
+    conn = ssl.wrap_socket(conn, keyfile=KEY_FILE, certfile=CERT_FILE, server_side=True)
 
-    # Create a socket to transfer data
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.connect((IP_ADDRESS, PORT))
+    def on_press(key):
+        try:
+            key_data = {'type': 'key', 'event': 'down', 'key_code': key.vk}
+            conn.sendall(json.dumps(key_data).encode('utf-8') + b'\n')
+        except (BrokenPipeError, ConnectionResetError, OSError):
+            return False
 
-        # Register input devices with a selector
-        selector = DefaultSelector()
-        if keyboard:
-            selector.register(keyboard, EVENT_READ)
-        if mouse:
-            selector.register(mouse, EVENT_READ)
+    def on_release(key):
+        try:
+            key_data = {'type': 'key', 'event': 'up', 'key_code': key.vk}
+            conn.sendall(json.dumps(key_data).encode('utf-8') + b'\n')
+        except (BrokenPipeError, ConnectionResetError, OSError):
+            return False
 
-        while True:
-            for key, mask in selector.select():
-                device = key.fileobj
-                event = device.read_one()
+    def on_move(x, y):
+        try:
+            mouse_data = {'type': 'mouse', 'event': 'move', 'x': x, 'y': y}
+            conn.sendall(json.dumps(mouse_data).encode('utf-8') + b'\n')
+        except (BrokenPipeError, ConnectionResetError, OSError):
+            return False
 
-                if event.type == evdev.ecodes.EV_KEY:  # Keyboard events
-                    device_type = 0x01
-                    event_type = 0x01 if event.value == 1 else 0x02
-                    data = struct.pack('>BBh', device_type, event_type, event.code)
-                    sock.sendall(data)
+    def on_click(x, y, button, pressed):
+        try:
+            button_data = {'type': 'mouse', 'event': 'click', 'x': x, 'y': y, 'button': str(button), 'pressed': pressed}
+            conn.sendall(json.dumps(button_data).encode('utf-8') + b'\n')
+        except (BrokenPipeError, ConnectionResetError, OSError):
+            return False
 
-                if event.type == evdev.ecodes.EV_REL:  # Mouse events
-                    if event.code in (ecodes.REL_X, ecodes.REL_Y):
-                        device_type = 0x02
-                        event_type = 0x03  # Move
-                        button = 0x00
-                        x, y = mouse.position()
-                        data = struct.pack('>BBhii', device_type, event_type, button, x, y)
-                        sock.sendall(data)
+    def on_scroll(x, y, dx, dy):
+        try:
+            scroll_data = {'type': 'mouse', 'event': 'scroll', 'x': x, 'y': y, 'dx': dx, 'dy': dy}
+            conn.sendall(json.dumps(scroll_data).encode('utf-8') + b'\n')
+        except (BrokenPipeError, ConnectionResetError, OSError):
+            return False
 
-                if event.type == evdev.ecodes.EV_BTN:  # Mouse button events
-                    device_type = 0x02
-                    event_type = 0x01 if event.value == 1 else 0x02
-                    button = event.code - ecodes.BTN_MOUSE
-                    x, y = mouse.position()
-                    data = struct.pack('>BBhii', device_type, event_type, button, x, y)
-                    sock.sendall(data)
+    with keyboard.Listener(on_press=on_press, on_release=on_release) as keyboard_listener, mouse.Listener(on_move=on_move, on_click=on_click, on_scroll=on_scroll) as mouse_listener:
+        keyboard_listener.join()
+        mouse_listener.join()
+
+    conn.close()
+    sock.close()
+
 
 def main():
-    audio_device_index = int(sys.argv[1])
-    camera_device_index = int(sys.argv[2])
+    generate_self_signed_cert(CERT_FILE, KEY_FILE)
 
-    # Threads for transferring audio and video streams
-    audio_thread = threading.Thread(target=transfer_audio_stream, args=(audio_device_index,))
-    video_thread = threading.Thread(target=transfer_video_stream, args=(camera_device_index,))
-    input_devices_thread = threading.Thread(target=transfer_input_devices)
+    # Register services
+    zeroconf_audio, service_info_audio = register_audio_stream()
+    zeroconf_video, service_info_video = register_video_stream()
+    zeroconf_input, service_info_input = register_input_devices()
 
-    audio_thread.start()
-    video_thread.start()
-    input_devices_thread.start()
+    # New line to register the Samba share
+    zeroconf_samba, service_info_samba = register_samba_share()
 
-    # Bonjour service registration
-    service_type = "_usb_transfer._tcp.local."
-    service_name = f"USB_Transfer_Service_{socket.gethostname()}._usb_transfer._tcp.local."
+    transfer_audio_stream()
+    transfer_video_stream(0)
+    transfer_input_devices()
 
-    zeroconf = Zeroconf()
-    service_info = ServiceInfo(
-        type_=service_type,
-        name=service_name,
-        addresses=[socket.inet_aton(IP_ADDRESS)],
-        port=PORT,
-        properties={},
-        server=f"{socket.gethostname()}.local.",
-    )
+    # Unregister services before exiting
+    zeroconf_audio.unregister_service(service_info_audio)
+    zeroconf_video.unregister_service(service_info_video)
+    zeroconf_input.unregister_service(service_info_input)
 
-    zeroconf.register_service(service_info)
+    # New line to unregister the Samba share
+    zeroconf_samba.unregister_service(service_info_samba)
 
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        zeroconf.unregister_service(service_info)
-        zeroconf.close()
+    zeroconf_audio.close()
+    zeroconf_video.close()
+    zeroconf_input.close()
+    zeroconf_samba.close()
 
 if __name__ == '__main__':
     main()
